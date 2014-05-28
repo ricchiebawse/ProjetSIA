1fd3b38d040c4321422ff3fb5c15628cae71d45e
ricchiebawse <ricchie@192.168.0.3>
2014-05-28 22:50:49 +0200

    DONE : Fin de Task4 - US4 (IHM) TODO : US5 --> IHM : Sélection de fichier .XML 		Proposer une première fenêtre avec champs Parcourir, puis rédirection avec bouton "Ouvrir" vers la JFrame IHMFrame, en  assignant à la variable globale file_name le name du fichier ouvert.

--- PublicationAnalytics/src/fr/dauphine/publications_analytics/src/Task4/IHM/IHMFrame.java
+++ PublicationAnalytics/src/fr/dauphine/publications_analytics/src/Task4/IHM/IHMFrame.java
@@ -1,8 +1,12 @@
 package fr.dauphine.publications_analytics.src.Task4.IHM;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.EventQueue;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import fr.dauphine.publications_analytics.src.Task3.*;
 
 import javax.swing.JFrame;
@@ -10,9 +14,12 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JList;
 
 import fr.dauphine.publications_analytics.src.Task3.XMLPublicationTask3;
+import fr.dauphine.publications_analytics.src.Task4.UserStory1;
+
 import javax.swing.JScrollPane;
 import javax.swing.JPopupMenu;
 import java.awt.Component;
@@ -23,60 +30,121 @@ import java.awt.event.ActionEvent;
 
 public class IHMFrame extends JFrame {
 
-	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					IHMFrame frame = new IHMFrame();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
-	public IHMFrame() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		UserStory3 us3 = new UserStory3();
-		
-		JScrollPane scrollPane = new JScrollPane();
+    private int currentCard = 1;
+    private ArrayList<JList> listJlist;
+    private JPanel cardPanel;
+    private CardLayout cl;
+    private UserStory1 us;
+    private JButton b2;
+    private String file_name;
+    
+    private void setList(ArrayList<String> author, JPanel p, String authorName, int currentPanel){
+    	//Ajout de la liste des co-auteurs de author dans la JList de la JPanel p		
+        if(p.getComponentCount()>1){//Si le JPanel possède déjà une JScrollPane (content une JList)
+        	p.remove(1);//On supprime cette JScrollPane pour la remplacer par une nouvelle
+        }
+        JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(29, 21, 170, 251);
-		contentPane.add(scrollPane);
-		JList list = new JList(us3.get_data_by_type("../dblp_curated_sample.xml", "author", "*").toArray());
-		scrollPane.setViewportView(list);
+		p.add(scrollPane);
 		
-		JButton btnSeeCoauthors = new JButton("See co-authors");
-		btnSeeCoauthors.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				try {
-					//TODO:
-					//En cliquant sur le bouton, on ré-exécute la fonction avec l'author selectionné, et on
-					//Redirige vers la même Frame, mais cette fois-ci avec le nouveau résultat de la fonction.
-					IHMFrame frame1 = new IHMFrame();
-					//setVisible(false);
-					frame1.setVisible(true);
-				} catch (Exception ee) {
-					ee.printStackTrace();
-				}
-			}
-		});
-		btnSeeCoauthors.setBounds(211, 110, 158, 29);
-		contentPane.add(btnSeeCoauthors);
-	}
+		//On recherche les co-auteurs de l'auteur sélectionné
+		Map<String,List<String>> test = us.get_cohauthor_by_data(file_name,author,"*");
+		JList list = new JList(test.get(authorName).toArray());
+		if(listJlist.size()<=currentPanel){
+			listJlist.add(list);
+		}
+		else{
+			listJlist.set(currentPanel, list);
+		}
+		scrollPane.setViewportView(list);
+	
+    }
+    public IHMFrame() {
+    		us = new UserStory1();	   
+    		file_name="../dblp_US2-3.xml";
 
+            setTitle("ISI2");
+            setSize(500, 300);
+            cardPanel = new JPanel();
+
+            cl = new CardLayout();
+            cardPanel.setLayout(cl);
+            JPanel p1 = new JPanel();
+    		
+            JPanel p2 = new JPanel();
+            JPanel p3 = new JPanel();
+            JPanel p4 = new JPanel();
+            JLabel lab1 = new JLabel("");
+            JLabel lab2 = new JLabel("");
+            JLabel lab3 = new JLabel("");
+            JLabel lab4 = new JLabel("");
+            p1.add(lab1);
+            p2.add(lab2);
+            p3.add(lab3);
+            p4.add(lab4);
+            
+	    		//Ajout de la liste de TOUS les auteurs dans la JPanel1. La JPanel1 et sa JList ne varira JAMAIS.
+	    		JScrollPane scrollPane = new JScrollPane();
+	    		scrollPane.setBounds(29, 21, 170, 251);
+	    		p1.add(scrollPane);
+	    		JList list = new JList(us.get_data_by_type(file_name, "author", "*").toArray());
+	    		scrollPane.setViewportView(list);
+	    		listJlist = new ArrayList<JList>();
+	    		listJlist.add(list);
+
+            cardPanel.add(p1, "1");
+            cardPanel.add(p2, "2");
+            cardPanel.add(p3, "3");
+            cardPanel.add(p4, "4");
+            JPanel buttonPanel = new JPanel();
+            JButton b1 = new JButton("Previous");
+            b2 = new JButton("Next");
+            buttonPanel.add(b1);
+            buttonPanel.add(b2);
+            b1.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent arg0) {
+                    	//boutton previous
+                    	//On efface la JList du JPanel actuel, et on redirige vers la JPanel précédente.
+                            if (currentCard > 1) {
+                        			b2.setVisible(true);
+                                    ((JPanel)cardPanel.getComponent(currentCard-1)).remove(1);
+                                    currentCard -= 1;
+                                    cl.show(cardPanel, "" + (currentCard));
+                            }
+                    }
+            });
+
+            b2.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent arg0) {
+                    	//boutton next
+                    	//Modifier la JList du JPanel suivant.
+                            if (currentCard < 4) {
+	                                //On place dans la liste author le nom de l'author selectionné
+	                                ArrayList<String> author = new ArrayList<String>();
+	                                author.add((String) listJlist.get(currentCard-1).getSelectedValue());
+	                                 
+	                                //On exécute setList pour modifier la JList du JPanel fraîchement affiché (le JPanel++).
+	                                	// cardPanel.getComponent(currentCard-1)  : Récupère le JPanel     
+	                                setList(author, (JPanel)cardPanel.getComponent(currentCard), author.get(0), currentCard);
+	                               
+	                                //Changement de JPanel vers le JPanel++
+                                    currentCard += 1;
+                                    cl.show(cardPanel, "" + (currentCard));
+                                    if(currentCard>=4){
+                                    	b2.setVisible(false);
+                                    }
+
+
+                            }
+                    }
+            });
+            getContentPane().add(cardPanel, BorderLayout.NORTH);
+            getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+    }
+
+    public static void main(String[] args) {
+            IHMFrame cl = new IHMFrame();
+            cl.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            cl.setVisible(true);
+    }
 }
